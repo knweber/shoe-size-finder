41c1e51854000f767879c3f184caaf4b7e1fc0ba
Kris <kris.weber.1994@gmail.com>
2017-11-07 11:30:10 -0800

    setup beautiful soup, return list of available sizes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome('./chromedriver')
 driver.get("https://tjmaxx.tjx.com/store/jump/product/Western-Toe-Heel-Booties/1000263516?colorId=NS2857147&pos=1:1&Ntt=western%20toe%20heel%20booties")
 
-sizes = driver.find_elements_by_class_name("option-name")
+html = driver.page_source
 
-for available in sizes:
-    # size = available.getAttribute("innerHTML")
-    print(available)
+soup = BeautifulSoup(html, "html.parser")
+
+available_sizes = []
+
+for size in soup.find_all("span", class_="option-name"):
+    if size.contents[0] == '8':
+
+print(available_sizes)
 
 driver.quit()
