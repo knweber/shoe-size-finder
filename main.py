a27e8ab734a3ca9a3d96d79619cb1c6ced240ff9
Kris <kris.weber.1994@gmail.com>
2017-11-07 11:40:41 -0800

    import smtplib

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import smtplib
 
-driver = webdriver.Chrome('./chromedriver')
-driver.get("https://tjmaxx.tjx.com/store/jump/product/Western-Toe-Heel-Booties/1000263516?colorId=NS2857147&pos=1:1&Ntt=western%20toe%20heel%20booties")
+def getSizes():
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get("https://tjmaxx.tjx.com/store/jump/product/Western-Toe-Heel-Booties/1000263516?colorId=NS2857147&pos=1:1&Ntt=western%20toe%20heel%20booties")
 
-html = driver.page_source
+    html = driver.page_source
 
-soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
-available_sizes = []
+    available_sizes = []
 
-for size in soup.find_all("span", class_="option-name"):
-    if size.contents[0] == '8':
+    for size in soup.find_all("span", class_="option-name"):
+        if size.contents[0] == '8':
 
-print(available_sizes)
+    print(available_sizes)
 
-driver.quit()
+    driver.quit()
+
+def sendEmail()
