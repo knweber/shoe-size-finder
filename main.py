e27af3b46e3dde73e2a6ca0be89c2a01ae25d120
Kris <kris.weber.1994@gmail.com>
2017-11-07 12:29:58 -0800

    Set up scheduler to run everyday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import smtplib
+from smtplib import SMTP as SMTP
+import schedule
+import time
 
 def getSizes():
     driver = webdriver.Chrome('./chromedriver')
@@ -16,20 +18,25 @@ def getSizes():
         if size.contents[0] == '8':
             sendEmail()
 
-    print(available_sizes)
-
     driver.quit()
 
 def sendEmail():
-    user = "user@gmail.com"
-    pswrd = "mypassword"
-    message = "Your size is available! Go to 'https://tjmaxx.tjx.com/store/jump/product/Western-Toe-Heel-Booties/1000263516?colorId=NS2857147&pos=1:1&Ntt=western%20toe%20heel%20booties' now!"
-    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 587)
-    server_ssl.ehlo()
-    server_ssl.login(user,pswrd)
-    server_ssl.sendmail(user,user,message)
-    server_ssl.close()
-    print "Email sent!"
+    user = "<YOUR_EMAIL>"
+    pswrd = "<YOUR_PASSWORD>"
+    message = "Your size is available! Go get those boots."
+    conn = SMTP(host="smtp.gmail.com", port=587)
+    conn.ehlo()
+    conn.starttls()
+    conn.ehlo()
+    sender = user
+    recipient = user
+    conn.login(user,pswrd)
+    conn.sendmail(sender,recipient,message)
+    print("Email sent!")
+    conn.close()
 
-if __name__ == "__main__":
-    getSizes()
+schedule.every().day.at("07:00").do(getSizes)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
