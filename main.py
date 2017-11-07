d13016de98fe2e26e77503b8d1a71c923dd4e795
Kris <kris.weber.1994@gmail.com>
2017-11-07 10:39:13 -0800

    add chromedriver

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('C:/Users/gndweber/Downloads/chromedriver')
 driver.get("https://tjmaxx.tjx.com/store/jump/product/Western-Toe-Heel-Booties/1000263516?colorId=NS2857147&pos=1:1&Ntt=western%20toe%20heel%20booties")
 
-results = driver.find_elements_by_class_name("option-name")
+sizes = driver.find_elements_by_class_name("option-name")
+
+for size in sizes:
+    print(size)
+
+driver.quit()
