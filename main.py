67ac85d8ea48bf7ae1ed67b61c8a3f0bdbc4f9f0
Kris <kris.weber.1994@gmail.com>
2017-11-07 10:40:52 -0800

    Add chromedriver binary to root folder in project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:/Users/gndweber/Downloads/chromedriver')
+driver = webdriver.Chrome('./chromedriver')
 driver.get("https://tjmaxx.tjx.com/store/jump/product/Western-Toe-Heel-Booties/1000263516?colorId=NS2857147&pos=1:1&Ntt=western%20toe%20heel%20booties")
 
 sizes = driver.find_elements_by_class_name("option-name")
